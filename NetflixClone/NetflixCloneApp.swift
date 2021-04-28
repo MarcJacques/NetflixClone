c5b48ff5c2ada8dfface582fa37d8a29b0c41333
Marc <marc.daniel.jacques@gmail.com>
2021-04-28 02:28:09 -0400

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Marc Jacques on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
